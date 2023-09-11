3dce6e7532a50dd88c2ea26e116fcd251ea0eb2f
ttybitnik <vinicius.moraes@eternodevir.com>
2023-09-11 18:04:05 -0300

    style(init): remove unnecessary space

--- init.el
+++ init.el
@@ -1,7 +1,7 @@
 (require 'package)
 (setq package-enable-at-startup nil)
 
-(add-to-list 'package-archives '( "melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 
 (package-initialize)
