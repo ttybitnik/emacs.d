1d44f0a99cce2cbd285f5e6245a1c8985890a30e
ttybitnik <vinicius.moraes@eternodevir.com>
2023-09-11 23:41:34 -0300

    feat(init): set package-archive-priorities

--- init.el
+++ init.el
@@ -4,6 +4,11 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 
+(setq package-archive-priorities '(("gnu" . 4)
+				   ("nongnu" . 3)
+				   ("melpa-stable" . 2)
+				   ("melpa" . 1)))
+
 (package-initialize)
 
 (unless (package-installed-p 'use-package)
