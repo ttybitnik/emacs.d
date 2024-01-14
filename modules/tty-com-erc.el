131bd698baa1b151e44b1e48eb4fadabaf0a2239
ttybitnik <vinicius.moraes@eternodevir.com>
2024-01-14 15:55:22 -0300

    feat(erc): remove deprecated sasl

--- modules/tty-com-erc.el
+++ modules/tty-com-erc.el
@@ -15,7 +15,7 @@
 ;;* Functions:
 
 (defun erc-login/ttybitnik ()
-  "ERC libera.chat login with SASL."
+  "ERC libera.chat TLS login."
   (interactive)
   (erc-tls :server "irc.libera.chat" :port "6697"))
 
@@ -26,27 +26,6 @@ Optional ARG is the argument passed to the original function."
   (funcall orig-fun arg)
   (when (eq major-mode 'erc-mode)
     (goto-char (point-max))))
-
-(defun erc-login ()
-  "Perform user authentication at the IRC server (PATCHED)."
-  (erc-log (format "login: nick: %s, user: %s %s %s :%s"
-                   (erc-current-nick)
-                   (user-login-name)
-                   (or erc-system-name (system-name))
-                   erc-session-server
-                   erc-session-user-full-name))
-  (if erc-session-password
-      (erc-server-send (format "PASS %s" erc-session-password))
-    (message "Logging in without password"))
-  (when (and (featurep 'erc-sasl) (erc-sasl-use-sasl-p))
-    (erc-server-send "CAP REQ :sasl"))
-  (erc-server-send (format "NICK %s" (erc-current-nick)))
-  (erc-server-send
-   (format "USER %s %s %s :%s"
-           (if erc-anonymous-login erc-email-userid (user-login-name))
-           "0" "*"
-           erc-session-user-full-name))
-  (erc-update-mode-line))
 
 ;;* Main:
 
@@ -85,11 +64,7 @@ Optional ARG is the argument passed to the original function."
 (setq erc-track-exclude-types '("JOIN" "PART" "NICK" "MODE" "AWAY" "QUIT"
 				"324" "329" "332" "333" "353" "477"))
 
-(load-file (expand-file-name "erc-sasl.el" local-d/ttybitnik))
-(require 'erc-sasl)
-
 (advice-add 'erc-track-switch-buffer :around #'erc-switch-buffer/ttybitnik)
-(add-to-list 'erc-sasl-server-regexp-list "irc\\.libera\\.chat")
 
 ;;* Bindings:
 
