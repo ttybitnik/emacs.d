31bb1abec25683394b89c455b697c3e64e44d5a7
ttybitnik <vinicius.moraes@eternodevir.com>
2024-01-16 14:22:16 -0300

    feat(erc): add minimal distraction and suppress

--- modules/tty-com-erc.el
+++ modules/tty-com-erc.el
@@ -12,6 +12,9 @@
 
 ;;* Variables:
 
+(defvar erc-suppress-list/ttybitnik
+  "List of channels for suppressing common messages track.")
+
 ;;* Functions:
 
 (defun erc-login/ttybitnik ()
@@ -26,6 +29,28 @@ Optional ARG is the argument passed to the original function."
   (funcall orig-fun arg)
   (when (eq major-mode 'erc-mode)
     (goto-char (point-max))))
+
+(defun erc-minimal-distraction/ttybitnik ()
+  "Toggle mode line display of common ERC activity.
+When enabled, only priority faces will be displayed in the mode line track,
+filtering out less important messages.  When disabled, all faces and messages
+will be displayed."
+  (interactive)
+  (if (eq erc-track-priority-faces-only nil)
+      (setq erc-track-priority-faces-only erc-suppress-list/ttybitnik)
+    (setq erc-track-priority-faces-only nil))
+  (message "ERC minimal distraction is now %s"
+           (if erc-track-priority-faces-only "enabled" "disabled")))
+
+(defun erc-suppress-generate/ttybitnik()
+ "Generate a list for `erc-track-priority-faces-only'.
+This function filters the ERC channels obtained from
+`erc-autojoin-channels-alist' and excludes channels listed in
+`erc-priority-list/ttybitnik'.  The resulting list represents
+channels that will have common messages suppressed in the mode line'."
+  (seq-remove (lambda (item)
+                (member item erc-priority-list/ttybitnik))
+	      (apply #'append erc-autojoin-channels-alist)))
 
 ;;* Main:
 
@@ -47,7 +72,8 @@ Optional ARG is the argument passed to the original function."
 (setq erc-log-channels-directory
       (expand-file-name (file-name-as-directory "logs") orpheusmx-d/ttybitnik))
 (setq erc-save-buffer-on-part t)
-(setq erc-track-priority-faces-only 'all)
+(setq erc-suppress-list/ttybitnik (erc-suppress-generate/ttybitnik))
+(setq erc-track-priority-faces-only erc-suppress-list/ttybitnik)
 (setq erc-track-faces-priority-list '(erc-error-face
                                       erc-current-nick-face
                                       erc-keyword-face
@@ -68,7 +94,8 @@ Optional ARG is the argument passed to the original function."
 
 ;;* Bindings:
 
-(global-set-key (kbd "C-c i") 'erc-login/ttybitnik)
+(global-set-key (kbd "C-c i i") 'erc-login/ttybitnik)
+(global-set-key (kbd "C-c i d") 'erc-minimal-distraction/ttybitnik)
 
 ;;* Hooks:
 
