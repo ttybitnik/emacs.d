417b5bada3a7b9d62422c1b6d50a89ca441ac89b
ttybitnik <vinicius.moraes@eternodevir.com>
2024-01-16 18:38:47 -0300

    feat(keycast): deactivate spaceline support

--- modules/tty-vis-keycast.el
+++ modules/tty-vis-keycast.el
@@ -14,22 +14,22 @@
 
 ;;* Main:
 
-(setq keycast-mode-line-format "%3s%k%c%r%3s")
+;; (setq keycast-mode-line-format "%3s%k%c%r%3s")
 
-(defalias 'keycast-mode 'keycast-mode/ttybitnik)
+;; (defalias 'keycast-mode 'keycast-mode/ttybitnik)
 
-(define-minor-mode keycast-mode/ttybitnik
-  "Show current command and its key binding in the mode line.
-This is a reimplementation of `keycast-mode' for use by users
-of the `spaceline' package."
-  :global t
-  (if keycast-mode/ttybitnik
-      (progn
-	  (add-to-list 'global-mode-string 'keycast-mode-line)
-	  (add-hook 'pre-command-hook 'keycast--update t)
-	  (keycast-mode-line-mode))
-    (remove-hook 'pre-command-hook 'keycast--update)
-    (setq global-mode-string (remove 'keycast-mode-line global-mode-string))))
+;; (define-minor-mode keycast-mode/ttybitnik
+;;   "Show current command and its key binding in the mode line.
+;; This is a reimplementation of `keycast-mode' for use by users
+;; of the `spaceline' package."
+;;   :global t
+;;   (if keycast-mode/ttybitnik
+;;       (progn
+;; 	  (add-to-list 'global-mode-string 'keycast-mode-line)
+;; 	  (add-hook 'pre-command-hook 'keycast--update t)
+;; 	  (keycast-mode-line-mode))
+;;     (remove-hook 'pre-command-hook 'keycast--update)
+;;     (setq global-mode-string (remove 'keycast-mode-line global-mode-string))))
 
 ;;* Bindings:
 
