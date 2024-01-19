66896b7b98e90a1bd17c25f29bc3bfb9b31696e4
ttybitnik <vinicius.moraes@eternodevir.com>
2024-01-19 12:58:42 -0300

    feat(erc): set notifications

--- modules/tty-com-erc.el
+++ modules/tty-com-erc.el
@@ -86,7 +86,7 @@ channels that will have common messages suppressed in the mode line'."
 (setq erc-modules '(autoaway autojoin button completion fill irccontrols
                              keep-place list match menu move-to-prompt netsplit
                              networks noncommands readonly ring stamp track
-                             hl-nicks scrolltobottom log))
+                             hl-nicks scrolltobottom log notifications))
 (setq erc-lurker-hide-list '("JOIN" "PART" "QUIT"))
 (setq erc-track-exclude-types '("JOIN" "PART" "NICK" "MODE" "AWAY" "QUIT"
 				"324" "329" "332" "333" "353" "477"))
