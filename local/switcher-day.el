83320c2f9d04780a18bd25e7ea204818f50c88ff
Vinícius Moraes <vinicius.moraes@eternodevir.com>
2025-10-01 20:35:49 -0300

    chore(switcher): remove deprecated files

--- local/switcher-day.el
+++
@@ -1,5 +0,0 @@
-;; (setq dashboard-startup-banner
-;;       (expand-file-name "dashLogo_blue.png" img-d/ttybitnik))
-(dashboard-refresh-buffer)
-(load-theme 'modus-operandi-tinted t)
-(match-current-line-background/ttybitnik)
--- local/switcher-night.el
+++
@@ -1,5 +0,0 @@
-;; (setq dashboard-startup-banner
-;;       (expand-file-name "dashLogo_green.png" img-d/ttybitnik))
-(dashboard-refresh-buffer)
-(load-theme 'modus-vivendi-tinted t)
-(match-current-line-background/ttybitnik)
