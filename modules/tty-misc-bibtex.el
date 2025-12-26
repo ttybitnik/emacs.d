fff4968b0390aa08af515c11ba7fdc1f704cd0eb
Vinícius Moraes <vinicius.moraes@eternodevir.com>
2025-12-25 23:42:02 -0300

    feat(bibtex): reconfigure completion

--- modules/tty-misc-bibtex.el
+++ modules/tty-misc-bibtex.el
@@ -27,6 +27,7 @@
 ;;* Functions:
 
 ;;* Main:
+;; TODO: Update values to new Calibre database keys.
 
 (setq bibtex-completion-bibliography
       `(,(expand-file-name "Library/general.bib" prometheus-d/ttybitnik)))
@@ -35,33 +36,42 @@
 			               prometheus-d/ttybitnik)))
 (setq bibtex-completion-notes-path
       (expand-file-name (file-name-as-directory "org-roam/ref")))
-(setq bibtex-completion-pdf-extension '(".pdf" ".epub" ".mobi")) ;; experimental
+
+(setq bibtex-dialect 'biblatex)
+(setq bibtex-completion-pdf-extension '(".pdf" ".epub" ".mobi"))
 (setq bibtex-completion-pdf-field "file")
 (setq bibtex-completion-pdf-symbol "P")
 (setq bibtex-completion-notes-symbol "N")
-(setq bibtex-dialect 'biblatex)
 (setq bibtex-completion-cite-default-command "cite")
 ;; (setq bibtex-completion-pdf-open-function 'helm-open-file-with-default-tool)
-(setq bibtex-completion-additional-search-fields
-      '(tags custom_materia custom_yesno custom_organizer
-	         custom_fichamento languages))
+;; (setq bibtex-completion-additional-search-fields
+;;       '(tags custom_materia custom_yesno custom_organizer
+;; 	         custom_fichamento languages))
+
 (setq bibtex-completion-format-citation-functions
-      '((latex-mode    . bibtex-completion-format-citation-cite)
-	    ;; (org-mode      . org-ref-helm-bibtex-insert-citation) ;; experimental
-	    (markdown-mode . bibtex-completion-format-citation-pandoc-citeproc)
-	    (default       . bibtex-completion-format-citation-default)))
-(setq bibtex-completion-display-formats
-      '((t . "${=has-pdf=:1}${=has-note=:1} ${year:4} ${author:25} ${title:*}
-${tags:25} ${languages:6} ${custom_yesno:1} ${custom_fichamento:9}
-${custom_organizer:10} ${custom_materia:25}")))
-(setq bibtex-user-optional-fields
-      '(("languages" "Languages")
-	    ("custom_fichamento" "Note-taking status")
-	    ("custom_organizer" "File type")
-	    ("custom_yesno" "Reading status")
-	    ("custom_materia" "Custom category")
-	    ("tags" "Personal tags")
-	    ("file" "File name")))
+      '((org-mode . bibtex-completion-format-citation-org-cite)  ;; not sure about this
+        (latex-mode . bibtex-completion-format-citation-cite)
+        (LaTeX-mode . bibtex-completion-format-citation-cite)
+        (markdown-mode . bibtex-completion-format-citation-pandoc-citeproc)
+        (python-mode . bibtex-completion-format-citation-sphinxcontrib-bibtex)
+        (rst-mode . bibtex-completion-format-citation-sphinxcontrib-bibtex)
+        (default . bibtex-completion-format-citation-default)))
+
+;; (setq bibtex-completion-display-formats
+;;       '((t . "${=has-pdf=:1}${=has-note=:1} ${year:4} ${author:25} ${title:*}
+;; ${tags:25} ${languages:6} ${custom_yesno:1} ${custom_fichamento:9}
+;; ${custom_organizer:10} ${custom_materia:25}")))
+;; (setq bibtex-user-optional-fields
+;;       '(("languages" "Languages")
+;; 	    ("custom_fichamento" "Note-taking status")
+;; 	    ("custom_organizer" "File type")
+;; 	    ("custom_yesno" "Reading status")
+;; 	    ("custom_materia" "Custom category")
+;; 	    ("tags" "Personal tags")
+;; 	    ("file" "File name")))
+
+
+(bibtex-completion-init)
 
 ;;* Bindings:
 
